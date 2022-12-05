6d5d51b44f43fee4713838f41b0c31874a5cf26e
NurulHudaDev <nurulhuda22dev@gmail.com>
2022-12-06 00:58:29 +0600

    our_services

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const InfoCard = ({img, cardTitle, cardDescription, bgClass}) => {
     return (
         <div>
-            <div class={`card lg:card-side bg-base-100 shadow-xl text-white pl-5 ${bgClass}`}>
+            <div class={`card lg:card-side shadow-xl text-white pl-5 ${bgClass}`}>
   <figure><img src={img} alt="Album"/></figure>
   <div class="card-body">
     <h2 class="card-title">{cardTitle}</h2>
